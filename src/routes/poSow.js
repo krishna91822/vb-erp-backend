871c84b9551a5e2008c4dc4d9d010e83a87043e2
yusuf <khanyusuf9044@gmail.com>
2021-11-30 17:18:33 +0530

    FEAT_CMS-16_Yusuf | Yusuf Shekh | Created Get API for PO/Sow

--- src/routes/poSow.js
+++ src/routes/poSow.js
@@ -3,10 +3,16 @@ const router = express.Router();
 
 const {
     createPoSow,
+    getPoSowList,
+    getPoDeatil,
+    getSortedPoList,
 } = require("../controllers/poSowController");
 
 
 router.post("/", createPoSow);
+router.get("/", getPoSowList);
+router.get("/:id", getPoDeatil)
+router.get("/sort/:fieldName", getSortedPoList)
 
 
 module.exports = router;
