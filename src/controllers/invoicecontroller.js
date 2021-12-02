f1b46ebad82e2cacd1072994ad6464b920f45bc7
yashdeepyadav <yashu.deepyadav@gmail.com>
2021-12-02 18:01:04 +0530

    Sprint 2 release preparation

---
+++ src/controllers/invoicecontroller.js
@@ -0,0 +1,151 @@
+const { invoiceschema } = require('../schema/invoiceschema')
+const Invoice = require('../models/invoicemodel')
+
+const newInvoice = async (req, res) => {
+  try {
+
+    // console.log("checking valiation")
+    // const { error } = invoiceschema.validate(req.body);
+
+    
+    // if (error) {
+    //   res.status(422).send(error);
+    // }
+
+    const invoice = await Invoice.create(req.body);
+    res.status(201).json({
+      status: true,
+      invoice,
+    })
+  } catch (error) {
+    res.status(422).send(error);
+  }
+};
+
+// const all_invoice = async (req, res) => {
+//   let invoices;
+//   try {
+//     invoices = await Invoice.find({}).populate('userid','Client_Name Project_Name Targetted_Resources PO_Number PO_Amount');
+//     res.status(200).json({
+//       status: true,
+//       invoices
+//     })
+//   } catch (error) {
+//     res.status(401).send(error);
+//   }
+// }
+
+const getInvoices = async (req, res) => {
+  try {
+    const getDetails = await Invoice.find({}).populate(
+      "userid",
+      "Client_Name Project_Name Targetted_Resources PO_Number PO_Amount"
+    );
+    res.status(200).json(getDetails);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
+const getInvoiceDetailsById = async (req, res) => {
+  try {
+    const getDetails = await Invoice.findById(req.params.id).populate(
+      "userid",
+      "Client_Name Project_Name Targetted_Resources PO_Number PO_Amount"
+    );
+    res.status(200).json(getDetails);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+const getInvoiceDetails = async (req, res) => {
+  try {
+    const data = req.params.data;
+    const val = "Client_Sponser";
+    const val2 = "Client_Name";
+    if (data === val) {
+      const details = await Invoice.find({})
+        .populate(
+          "userid",
+          "Client_Name Project_Name Targetted_Resources PO_Number PO_Amount"
+        )
+        .collation({ locale: "en" })
+        .sort(data);
+      res.status(200).json(details);
+    } else if (data === val2) {
+      const details = await Invoice.aggregate([
+        {
+          $lookup: {
+            from: "purchase_orders",
+            localField: "userid",
+            foreignField: "_id",
+            as: "purchase_orders",
+          },
+        },
+        { $unwind: "$purchase_orders" },
+        { $sort: { "purchase_orders.Client_Name": 1 } },
+      ]);
+      res.status(200).json(details);
+    } else {
+      const details = await Invoice.aggregate([
+        {
+          $lookup: {
+            from: "purchase_orders",
+            localField: "userid",
+            foreignField: "_id",
+            as: "purchase_orders",
+          },
+        },
+        { $unwind: "$purchase_orders" },
+        { $sort: { "purchase_orders.Project_Name": 1 } },
+      ]);
+      res.status(200).json(details);
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+module.exports = {  newInvoice ,getInvoices,getInvoiceDetailsById,getInvoiceDetails};
+
+// const update_invoice = async (req, res) => {
+//   let invoice = await Invoice.findById(req.params.id);
+
+//   if (!invoice) {
+//     return res.status(500).json({
+//       success: false,
+//       message: "Invoice not found"
+//     })
+//   }
+
+//   invoice = await Invoice.findByIdAndUpdate(req.params.id, req.body, {
+//     new: true,
+//     runValidators: true,
+//     useFindandModify: false
+//   });
+
+//   res.status(200).json({
+//     status: true,
+//     invoice
+//   })
+// }
+
+// const delete_invoice = async (req, res) => {
+//   const invoice = await Invoice.findById(req.params.id);
+
+//   if (!invoice) {
+//     return res.status(500).json({
+//       status: false,
+//       message: "Invoice not found"
+//     })
+//   }
+
+//   await invoice.remove();
+
+//   res.status(200).json({
+//     success: true,
+//     message: "Invoice deleted"
+//   })
+
+// }
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const userRoutes = require("./users");
 const rewardRoutes = require("./rewards");
 const poSowRoutes = require("./poSow")
 const assigneeRoutes = require("./assign")
+const invoiceRoutes = require("./invoiceroutes")
 const { auth, getAccount } = require("../controllers/userController");
 const { isAuthorized } = require("../middleware/auth");
 
@@ -12,7 +13,8 @@ router.post("/login", auth);
 router.get("/account", isAuthorized, getAccount);
 router.use("/users", isAuthorized, userRoutes);
 router.use("/rewards", rewardRoutes);
-router.use("/poSow", isAuthorized, poSowRoutes)
-router.use("/assign", isAuthorized,assigneeRoutes)
+router.use("/poSow",  poSowRoutes)
+router.use("/assign", assigneeRoutes)
+router.use("/invoice",invoiceRoutes)
 
 module.exports = router;
