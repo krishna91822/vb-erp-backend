b832945461dbebc07666add7a6c0cca2e6ce9ad1
Thanuja Alavala <thanujaalavala@Thanujas-MacBook-Air.local>
2021-12-02 16:19:37 +0530

    VR-26|Thanuja|rewards api with searching and pagination

--- src/schema/rewardSchema.js
+++ src/schema/rewardSchema.js
@@ -2,11 +2,11 @@ const Joi = require("joi");
 
 const rewardSchema = Joi.object()
   .keys({
-    reward_name: Joi.string().required(),
     reward_display_name: Joi.string().required(),
     reward_type: Joi.string().required(),
+    reward_subType: Joi.string(),
     reward_sender: Joi.string().required(),
-    recepients: Joi.array().required(),
+    reward_receiver: Joi.array().required(),
     receiver_message: Joi.string().required(),
     announcement_type: Joi.string().required(),
     slack_channel: Joi.string().required(),
