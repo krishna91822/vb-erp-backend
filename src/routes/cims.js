e5d24c99ce10bd5e380beb7c6571bc543bbac16e
Radhika Manglik <radhika.m@valuebound.com>
2021-12-06 13:29:09 +0000

    Update cims.js

--- src/routes/cims.js
+++ src/routes/cims.js
@@ -1,12 +1,13 @@
 const express=require("express");
 const jsonwebtoken=require("jsonwebtoken");
 const router = express.Router()
-const {cimsDel,cimsGet,cimsPatch,cimsPost}=require("../controllers/cimsController");
+const {cimsDel,cimsGet,cimsPatch,cimsPost,getbystatus}=require("../controllers/cimsController");
 
 router.get('/',authenticateToken,cimsGet)
 router.post('/', cimsPost)
 router.delete('/', cimsDel)
 router.patch('/', cimsPatch)
+router.get('/status',getbystatus)
 
 const token_secret = '6850cc6ab29180f03f647c9b7ff331298038b2cd9bf71980f87bfd547e0da37ac60c4c5d7f7136f81b81496a741f496ea3e528b70755bcf020874e0ef01446db'
 
