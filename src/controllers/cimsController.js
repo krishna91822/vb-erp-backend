a7ab7dc36cd4f6a1fc259f22fa2a5e06d253c00c
Neelakanta Gouda Patil <neelakanta.p@valuebound.com>
2021-12-08 12:41:08 +0530

    dummy

--- src/controllers/cimsController.js
+++ src/controllers/cimsController.js
@@ -3,179 +3,163 @@ const { cimsSchema, updateSchema } = require("../schema/cimsSchema");
 const { rewardSchema } = require("../schema/rewardSchema");
 const { customResponse } = require("../utility/helper");
 
+const getbystatus = async (req, res) => {
+  const { status } = req.headers;
 
-
-const getbystatus = async(req,res)=>{
-    const {status} = req.headers
-   
- compModal.find({status:[status]}).then(clientData=>res.send(clientData))
-//compModal.find({status:{$eq:status}}).then(clientData=>res.send(clientData))
-
-}
+  compModal
+    .find({ status: [status] })
+    .then((clientData) => res.send(clientData));
+  //compModal.find({status:{$eq:status}}).then(clientData=>res.send(clientData))
+};
 
 //Get all records in database
 const cimsGet = async (req, res) => {
+  try {
+    const Comps = await compModal.find({});
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
 
-    try {
-        const Comps = await compModal.find({});
-        const page = parseInt(req.query.page)
-        const limit = parseInt(req.query.limit)
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const count = await compModal.find().countDocuments();
 
-        const startIndex = (page - 1) * limit
-        const endIndex = page* limit
-        const count = await compModal.find().countDocuments()
+    const data = {};
+    data.data = Comps.slice(startIndex, endIndex);
+    data.totalPages = Math.ceil(count / limit);
 
-        const data = {}
-        data.data = Comps.slice(startIndex, endIndex)
-        data.totalPages = Math.ceil(count/limit)
-        
-        code = 200
-        message = "Data fetched successfully"
+    code = 200;
+    message = "Data fetched successfully";
 
-        const resData = customResponse({
-            data,
-            code,
-            message
-        })
-        res.send(resData);
+    const resData = customResponse({
+      data,
+      code,
+      message,
+    });
+    res.send(resData);
+  } catch (error) {
+    code = 422;
 
-    } catch (error) {
-
-        code = 422;
-
-        const resData = customResponse({
-            code,
-            error: error && error.details
-        });
-        return res.send(resData);
-    }
+    const resData = customResponse({
+      code,
+      error: error && error.details,
+    });
+    return res.send(resData);
+  }
 };
 
 //Post record in database
 const cimsPost = async (req, res) => {
-    
-    try {
-        const { error } = cimsSchema.validate(req.body);
+  try {
+    const { error } = cimsSchema.validate(req.body);
 
-        if (error) {
+    if (error) {
+      code = 422;
+      message = "Invalid request data";
 
-            code = 422;
-            message = "Invalid request data";
-
-            const resData = customResponse({
-                code,
-                message,
-                err: error && error.details,
-            });
-            return res.send(resData);
-
-        }
-        const newComp = await compModal.create(req.body);
-        
-        data = newComp
-        code = 200
-        message = "Data created successfully"
-
-        const resData = customResponse({
-            data,
-            code,
-            message
-        })
-        res.send(resData);
+      const resData = customResponse({
+        code,
+        message,
+        err: error && error.details,
+      });
+      return res.send(resData);
     }
-    catch (err) {
+    const newComp = await compModal.create(req.body);
 
-        code = 422;
-        const resData = customResponse({
-            code,
-            error: error && error.details
-        });
-        return res.send(resData);
-    }
+    data = newComp;
+    code = 200;
+    message = "Data created successfully";
+
+    const resData = customResponse({
+      data,
+      code,
+      message,
+    });
+    res.send(resData);
+  } catch (err) {
+    code = 422;
+    const resData = customResponse({
+      code,
+      error: error && error.details,
+    });
+    return res.send(resData);
+  }
 };
 
 //Delete record in database
 const cimsDel = async (req, res) => {
+  const { id } = req.query;
 
-    const { id } = req.query;
-    
-    try {
-        const del = await compModal.findById(id);
-        await del.remove();
+  try {
+    const del = await compModal.findById(id);
+    await del.remove();
 
-        code = 200;
-        message = "Data deleted successfully"
+    code = 200;
+    message = "Data deleted successfully";
 
-        const resData = customResponse({
-            code,
-            message,
-            error
-        });
-        res.send(resData)
-    } catch (error) {
+    const resData = customResponse({
+      code,
+      message,
+      error,
+    });
+    res.send(resData);
+  } catch (error) {
+    code = 422;
+    data = req.body;
 
-        code = 422;
-        data = req.body
-
-        const resData = customResponse({
-            code,
-            message,
-            error: error && error.details
-        });
-        return res.send(resData);
-    }
+    const resData = customResponse({
+      code,
+      message,
+      error: error && error.details,
+    });
+    return res.send(resData);
+  }
 };
 
 //Update record in database
 const cimsPatch = async (req, res) => {
+  //const _id = req.body._id;
+  //const { designation, brandname, clientname, domain, baselocation,address, status,contacts } = req.body;
 
-    //const _id = req.body._id;
-    //const { designation, brandname, clientname, domain, baselocation,address, status,contacts } = req.body;
-    
-    try {
-        const { error } = updateSchema.validate(req.body);
-        if (error) {
+  try {
+    const { error } = updateSchema.validate(req.body);
+    if (error) {
+      code = 422;
+      message = "Invalid request data";
 
-            code = 422;
-            message = "Invalid request data";
+      const resData = customResponse({
+        code,
+        message,
+        err: error && error.details,
+      });
+      return res.send(resData);
+    }
 
-            const resData = customResponse({
-                code,
-                message,
-                err: error && error.details,
-            });
-            return res.send(resData);
-
-        }
-
-       /* const update = await compModal.findOneAndUpdate({ _id: _id }, {
+    /* const update = await compModal.findOneAndUpdate({ _id: _id }, {
             designation: designation, brandname: brandname, clientname: clientname, domain: domain, baselocation: baselocation,status:status,
            address:address, contacts: contacts
         });*/
-         const update=await compModal.findOneAndUpdate(req.params.id,req.body)
+    const update = await compModal.findOneAndUpdate(req.params.id, req.body);
 
-        code = 200;
-        data = req.body
-        message = "Data updated successfully";
+    code = 200;
+    data = req.body;
+    message = "Data updated successfully";
 
-        const resData = customResponse({
-            code,
-            message,
-            data
-        });
-        return res.send(resData);
-    }
-    catch (error) {
+    const resData = customResponse({
+      code,
+      message,
+      data,
+    });
+    return res.send(resData);
+  } catch (error) {
+    code = 422;
 
-        code = 422;
-
-        const resData = customResponse({
-            code,
-            message,
-            error: error && error.details
-        });
-        res.send(resData)
-    }
+    const resData = customResponse({
+      code,
+      message,
+      error: error && error.details,
+    });
+    res.send(resData);
+  }
 };
 
-module.exports = { cimsDel, cimsGet, cimsPatch, cimsPost ,getbystatus}
+module.exports = { cimsDel, cimsGet, cimsPatch, cimsPost, getbystatus };
