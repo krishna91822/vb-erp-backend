aa9563326bc6ca3206309d2fb0d3f4ffb5b1a196
Adithya Pantangi <adithya.p@valuebound.com>
2021-12-09 14:59:11 +0530

    VET-56 reviews.js added

--- routes/ReviewRoutes.js
+++ routes/ReviewRoutes.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const reviewController = require('../controllers/ReviewController');
-const authController = require('./../controllers/authController');
+const reviewController = require("../controllers/ReviewController");
+const authController = require("./../controllers/authController");
 
 const router = express.Router();
 
 router
-  .route('/')
+  .route("/")
   .get(authController.protect, reviewController.getAllReviews)
   .post(authController.protect, reviewController.createReview); //Create Review (FOR ADMIN);
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(authController.protect, reviewController.getReview) //Get Review details (FOR READ ONLY)
   .patch(
     authController.protect,
@@ -20,7 +20,7 @@ router
   ) //Update Review details
   .delete(
     authController.protect,
-    authController.restrictTo('admin'),
+    authController.restrictTo("admin"),
     reviewController.deleteReview
   ); //delete Review documents
 
