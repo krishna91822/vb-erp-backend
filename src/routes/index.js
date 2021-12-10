ce474fc55280a36e1224bfded1cf7451ea85c5cc
Atif Kamal <atif.k@valuebound.com>
2021-12-10 21:54:52 +0530

    PMO-108 | Atif Kamal | Integration testing with CIMS module

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,13 +7,11 @@ const ProjectRouter = require("./projects");
 const EmployeeRouter = require("./employee");
 const ProjectEmployeeRouter = require("./projectAndEmployee");
 const cimsRoutes = require("./cims");
-const ClientRouter = require("./clients");
 
 //APIs
 router.use("/projects", ProjectRouter);
 router.use("/employees", EmployeeRouter);
 router.use("/allocations", ProjectEmployeeRouter);
 router.use("/cims", cimsRoutes);
-router.use("/clients", ClientRouter);
 
 module.exports = router;
