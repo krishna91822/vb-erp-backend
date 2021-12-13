942f1b5734a7a3531c97e75e92cfc06674dd4e29
paidimarri.a <paidimarri.a@valuebound.com>
2021-12-13 15:00:02 +0530

    PMO-93|Abhiram|nothing changed

--- src/schema/projectEmployeeSchema.js
+++ src/schema/projectEmployeeSchema.js
@@ -6,7 +6,7 @@ const projectEmployeeSchema = Joi.object()
     projectId: Joi.objectId(),
     empId: Joi.objectId(),
     allocationStartDate: Joi.string(),
-    primaryCapiblities:Joi.array(),
+    primaryCapiblities: Joi.array(),
     allocationEndDate: Joi.string(),
     allocationPercentage: Joi.number(),
     rackRate: Joi.number(),
