f09176de3a932e2e7217d9dc107edb594033d1c6
Saad <saad.h@valuebound.com>
2021-12-16 17:22:47 +0530

    PMO-97| Saad & Atif | minor changes

--- src/schema/projectsSchema.js
+++ src/schema/projectsSchema.js
@@ -1,26 +1,26 @@
 const Joi = require("joi");
 
 const projectsSchema = Joi.object()
-    .keys({
-        clientId: Joi.string(),
-        clientName: Joi.string().required(),
-        clientPrimaryContactName: Joi.string(),
-        projectName: Joi.string().required(),
-        clientProjectManager: Joi.string().required(),
-        startDate: Joi.string().required(),
-        endDate: Joi.string().required(),
-        clientProjectSponsor: Joi.string().required(),
-        clientFinanceController: Joi.string().required(),
-        clientPrimaryContact: Joi.number(),
-        vbProjectManager: Joi.string().required(),
-        domainSector: Joi.string(),
-        vbProjectStatus: Joi.string().valid(
-            "Un Assigned",
-            "On Hold",
-            "Done",
-            "Active"
-        ),
-    })
-    .options({ abortEarly: false });
+  .keys({
+    clientId: Joi.string(),
+    clientName: Joi.string().required(),
+    clientPrimaryContactName: Joi.string(),
+    projectName: Joi.string().required(),
+    clientProjectManager: Joi.string().required(),
+    startDate: Joi.string().required(),
+    endDate: Joi.string().required(),
+    clientProjectSponsor: Joi.string().required(),
+    clientFinanceController: Joi.string().required(),
+    clientPrimaryContact: Joi.number(),
+    vbProjectManager: Joi.string().required(),
+    domainSector: Joi.string(),
+    vbProjectStatus: Joi.string().valid(
+      "Yet to Begin",
+      "On Hold",
+      "Done",
+      "Active"
+    ),
+  })
+  .options({ abortEarly: false });
 
 module.exports = { projectsSchema };
