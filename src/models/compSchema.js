001a02ea9f145398dd5c8c123a293cf0a62a223d
sam7468 <samsharan7468@gmail.com>
2021-12-17 01:13:11 +0530

    000 | SAM | done with all merges

--- src/models/compSchema.js
+++ src/models/compSchema.js
@@ -4,20 +4,12 @@ const mongoose = require("mongoose");
 const compSchema = mongoose.Schema(
   {
     rowNumber: Number,
-<<<<<<< HEAD
     legalName: String,
     brandName: { type: String, unique: true },
 
     domain: String,
     // Need to verify this field
     // baseLocation: String,
-=======
-    designation: String,
-    brandName: { type: String, unique: true },
-    clientName: String,
-    domain: String,
-    baseLocation: String,
->>>>>>> 71f2ffda8f75b5f7c80e13cf28ae91f37b43e83d
     gstNumber: String,
     panNumber: String,
     companyType: String,
@@ -29,11 +21,7 @@ const compSchema = mongoose.Schema(
       country: String,
       state: String,
       district: String,
-<<<<<<< HEAD
       area: String,
-=======
-      city: String,
->>>>>>> 71f2ffda8f75b5f7c80e13cf28ae91f37b43e83d
       landmark: String,
     },
     communicationAddress: {
@@ -43,26 +31,13 @@ const compSchema = mongoose.Schema(
       country: String,
       state: String,
       district: String,
-<<<<<<< HEAD
       area: String,
-=======
-      city: String,
->>>>>>> 71f2ffda8f75b5f7c80e13cf28ae91f37b43e83d
       landmark: String,
     },
     contacts: Object,
     status: {
-<<<<<<< HEAD
       type: Number,
       default: 1,
-=======
-      type: String,
-      enum: {
-        values: [0, 1],
-        message: "0 for inactive and 1 for active",
-      },
-      default: "1",
->>>>>>> 71f2ffda8f75b5f7c80e13cf28ae91f37b43e83d
     },
   },
 
