d978d556b42e99a9d9bba3fcf54e404ba0cb2caa
Atif Kamal <atif.k@valuebound.com>
2021-12-16 20:11:35 +0530

    PMO-10 | Atif Kamal | Created seperate API for fetching Managers

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -2,13 +2,13 @@ const express = require("express");
 const EmployeeRouter = express.Router();
 const {
     getFilteredEmp,
-    getVbManagers,
+    getManagers,
 } = require("../controllers/employeeController");
 
 // GET request
 EmployeeRouter.get("/filteremp", getFilteredEmp);
 
 // GET request
-EmployeeRouter.get("/empManagers", getVbManagers);
+EmployeeRouter.get("/empManagers", getManagers);
 
 module.exports = EmployeeRouter;
