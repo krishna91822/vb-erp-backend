dbd90c73cb7d10c26dabacad200e771abf9b7d9b
sam7468 <samsharan7468@gmail.com>
2021-12-17 13:36:11 +0530

    000 | SAM | deleted duplicated employeeRoutes

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -570,17 +570,9 @@ exports.getFilteredEmp = async (req, res) => {
 exports.getManagers = async (req, res) => {
   const filterManagers = await EmployeeInfoModel.find(
     {
-      $or: [
-        {
-          empDesignation: "Manager",
-          empName: {
-            $regex: req.query.empName,
-            $options: "i",
-          },
-        },
-      ],
+      empDesignation: "Manager",
     },
-    { _id: 0, empName: 1 }
+    { empName: 1 }
   );
   return res.status(200).send(filterManagers);
 };
