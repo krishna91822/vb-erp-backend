18ab009759c722a8fe5ecee015a03dd1e5308a5e
sam7468 <samsharan7468@gmail.com>
2021-12-17 08:47:23 +0530

    000 | SAM | resolved db conflicts | updated db.js

--- src/utility/db.js
+++ src/utility/db.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
-const DB =
-  process.env.NODE_ENV === "test"
-    ? process.env.TEST_DATABASE_URI
-    : process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+const DB = process.env.MONGO_URL;
 // process.env.NODE_ENV === "test"
 //   ? process.env.TEST_DATABASE_URI
-//   : process.env.DATABASE_URL;
+//   : process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+// process.env.NODE_ENV === "test";
+// ? process.env.TEST_DATABASE_URI
+// : process.env.DATABASE_URL;
 
 const mongoose = require("mongoose");
 const db = mongoose.connection;
 
 const connectToDb = () => {
-  mongoose.connect(DB);
-  console.log(`DB is ${DB}`);
+  mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   db.on("error", console.error.bind(console, "connection error: "));
   db.on("open", console.error.bind(console, "DB connected: "));
   db.on("close", console.error.bind(console, "Connection closed"));
