afa9c6d6637b59ad113bf974c319e60e4f93d3f3
Saad <saad.h@valuebound.com>
2021-12-17 18:42:48 +0530

    Saad Hasan | Make another route for ON HOld and yet to begin

--- src/controllers/projectsController.js
+++ src/controllers/projectsController.js
@@ -182,16 +182,7 @@ const getSortedActiveProjects = async (req, res) => {
     code = 200;
     message = "Data Fetched Successfully!!";
     const updatedProjects = await ProjectsInfoModel.find({
-      $and: [
-        {
-          $and: query,
-          $or: [
-            { vbProjectStatus: "On Hold" },
-            { vbProjectStatus: "Active" },
-            { vbProjectStatus: "Yet to Begin" },
-          ],
-        },
-      ],
+      $and: [{ $and: query }, { vbProjectStatus: "Active" }],
     }).sort(fieldName);
     const data = customPagination({ data: updatedProjects, page, limit });
     const resData = customResponse({ code, message, data });
@@ -295,6 +286,109 @@ const getSortedDoneProjects = async (req, res) => {
   }
 };
 
+//getting all the others  projects
+const getOtherProjects = async (req, res) => {
+  const query = getQueryString(req.query);
+  const page = req.query.page ? req.query.page : 1;
+  const limit = req.query.limit ? req.query.limit : 10;
+  let code, message;
+  try {
+    const Projects = await ProjectsInfoModel.find({});
+    await Projects.forEach(async (element) => {
+      let date = moment().format("YYYY-MM-DD");
+      date = moment(date, "YYYY-MM-DD");
+      let startDate = moment(element.startDate, "YYYY-MM-DD");
+      let endDate = moment(element.endDate, "YYYY-MM-DD");
+
+      if (element.vbProjectStatus == "On Hold" || "Yet to Begin") {
+      } else if (date.isAfter(endDate)) {
+        let updateElement = await ProjectsInfoModel.findOneAndUpdate(
+          { _id: element._id },
+          { $set: { vbProjectStatus: "Done" } }
+        );
+        updateElement.save();
+      }
+    });
+    code = 200;
+    message = "Data Fetched Successfully!!";
+    const updatedProjects = await ProjectsInfoModel.find({
+      $and: [
+        {
+          $and: query,
+          $or: [
+            { vbProjectStatus: "On Hold" },
+            { vbProjectStatus: "Yet to Begin" },
+          ],
+        },
+      ],
+    });
+    const data = customPagination({ data: updatedProjects, page, limit });
+    const resData = customResponse({ code, message, data });
+    res.status(200).send(resData);
+  } catch (error) {
+    code = 500;
+    message = "Internal server error";
+    const resData = customResponse({
+      code,
+      message,
+      err: error,
+    });
+    return res.status(code).send(resData);
+  }
+};
+
+//getting all the others sorted projects
+const getSortedOtherProjects = async (req, res) => {
+  const fieldName = req.params.fieldName;
+  const query = getQueryString(req.query);
+  const page = req.query.page ? req.query.page : 1;
+  const limit = req.query.limit ? req.query.limit : 10;
+  let code, message;
+  try {
+    const Projects = await ProjectsInfoModel.find({});
+    await Projects.forEach(async (element) => {
+      let date = moment().format("YYYY-MM-DD");
+      date = moment(date, "YYYY-MM-DD");
+      let startDate = moment(element.startDate, "YYYY-MM-DD");
+      let endDate = moment(element.endDate, "YYYY-MM-DD");
+
+      if (element.vbProjectStatus == "On Hold" || "Yet to Begin") {
+      } else if (date.isAfter(endDate)) {
+        let updateElement = await ProjectsInfoModel.findOneAndUpdate(
+          { _id: element._id },
+          { $set: { vbProjectStatus: "Done" } }
+        );
+        updateElement.save();
+      }
+    });
+    code = 200;
+    message = "Data Fetched Successfully!!";
+    const updatedProjects = await ProjectsInfoModel.find({
+      $and: [
+        {
+          $and: query,
+          $or: [
+            { vbProjectStatus: "On Hold" },
+            { vbProjectStatus: "Yet to Begin" },
+          ],
+        },
+      ],
+    }).sort(fieldName);
+    const data = customPagination({ data: updatedProjects, page, limit });
+    const resData = customResponse({ code, message, data });
+    res.status(200).send(resData);
+  } catch (error) {
+    code = 500;
+    message = "Internal server error";
+    const resData = customResponse({
+      code,
+      message,
+      err: error,
+    });
+    return res.status(code).send(resData);
+  }
+};
+
 //getting single project by its _id
 const getProjectById = async (req, res) => {
   try {
@@ -316,5 +410,7 @@ module.exports = {
   getSortedActiveProjects,
   getDoneProjects,
   getSortedDoneProjects,
+  getOtherProjects,
+  getSortedOtherProjects,
   getProjectById,
 };
