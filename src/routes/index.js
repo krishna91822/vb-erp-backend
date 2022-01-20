2902aa444e42cdf31ea84380402dac0f23dbbe28
Aquib Ahmad <aquib.ahmad@valuebound.com>
2022-01-20 09:38:49 +0530

    Authorization|Aquib Ahmad|protect routes based on permissions

--- src/routes/index.js
+++ src/routes/index.js
@@ -25,16 +25,16 @@ const universityRoutes = require("./universityRoute");
 router.post("/login", auth);
 router.get("/account", isAuthorized, getAccount);
 router.use("/users", isAuthorized, userRoutes);
-router.use("/rewards", isAuthorized, rewardRoutes); //
+router.use("/rewards", isAuthorized, rewardRoutes);
 router.use("/", otherRoutes);
-router.use("/assign", isAuthorized, assigneeRoutes); //doubt
-router.use("/invoice", isAuthorized, invoiceRoutes); //
-router.use("/poSow", isAuthorized, poSowRoutes); //
-router.use("/employees", isAuthorized, employeeRoutes); //doubt
-router.use("/reviews", isAuthorized, reviewRoutes); //doubt
-router.use("/projects", isAuthorized, ProjectRouter); //
-router.use("/allocations", isAuthorized, ProjectEmployeeRouter); //
-router.use("/cims", isAuthorized, cimsRoutes); //
+router.use("/assign", isAuthorized, assigneeRoutes);
+router.use("/invoice", isAuthorized, invoiceRoutes);
+router.use("/poSow", isAuthorized, poSowRoutes);
+router.use("/employees", isAuthorized, employeeRoutes);
+router.use("/reviews", isAuthorized, reviewRoutes);
+router.use("/projects", isAuthorized, ProjectRouter);
+router.use("/allocations", isAuthorized, ProjectEmployeeRouter);
+router.use("/cims", isAuthorized, cimsRoutes);
 router.use("/tempUsers", isAuthorized, tempUserRoutes);
 router.use("/roles", isAuthorized, rolesRoutes);
 router.use("/universities", isAuthorized, universityRoutes);
