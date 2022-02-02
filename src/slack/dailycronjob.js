7b9e20d6246cc6d0eb271ced3ee6e6003316ca64
Aquib Ahmad <aquib.ahmad@valuebound.com>
2022-02-02 19:39:39 +0530

    made some changes in slack script

--- src/slack/dailycronjob.js
+++ src/slack/dailycronjob.js
@@ -1,19 +1,34 @@
+require("dotenv").config();
 const { birthdayjob } = require("./operations");
 const { workanniversary } = require("./operations");
+const axios = require("axios");
 
 async function dailycronjob() {
   try {
-    console.log("Hi======================");
     await birthdayjob("Daily");
     await workanniversary("Daily");
   } catch (error) {
+    console.log(process.env.NODE_ENV, "here2");
     console.log(error.message);
   }
 }
 
-const run = async () => {
-  await dailycronjob();
+const runDailyScript = async () => {
+  const user = {
+    email: process.env.slackemail,
+    password: process.env.slackpassword,
+  };
+  try {
+    await axios.post(`${process.env.URL}/login`, user).then(async (res) => {
+      axios.defaults.headers.common["Authorization"] = res.data.data.token;
+      await dailycronjob();
+    });
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    await axios.get(`${process.env.URL}/logout`);
+  }
 };
-run();
+runDailyScript();
 
 module.exports = dailycronjob;
--- src/slack/monthlycrownjob.js
+++ src/slack/monthlycrownjob.js
@@ -1,4 +1,5 @@
 const { starOfTheMonth } = require("./operations");
+const axios = require("axios");
 
 const monthlyjob = async () => {
   try {
@@ -8,6 +9,22 @@ const monthlyjob = async () => {
   }
 };
 
-monthlyjob();
+const runMonthlyScript = async () => {
+  const user = {
+    email: process.env.slackemail,
+    password: process.env.slackpassword,
+  };
+  try {
+    await axios.post(`${process.env.URL}/login`, user).then(async (res) => {
+      axios.defaults.headers.common["Authorization"] = res.data.data.token;
+      await monthlyjob();
+    });
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    await axios.get(`${process.env.URL}/logout`);
+  }
+};
+runMonthlyScript();
 
 module.exports = monthlyjob;
